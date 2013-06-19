0883727549405f1e346741be500b1e453355e8ce
XenCoin <xencoin@gmail.com>
2013-06-19 11:31:52 +0300

    xencoin

---
+++ README.md
@@ -0,0 +1,36 @@
+Xencoin - a fork of Litecoin version with fast block time and faster confirmations (4 confirmations needed instead of 6). Like Litecoin it uses scrypt as a proof of work scheme.
+
+	- 20 seconds block target
+	- Difficulty retargets every 0.40 day
+	- Total coins will be around 2100 millions. 
+	- Each block will have 200 coins per block at the beginning, it will be halved every two years (or 3,11,400 blocks).
+	- The default ports are 4334(connect) and 4335(json rpc).
+
+
+Development process
+===================
+
+Developers work in their own trees, then submit pull requests when
+they think their feature or bug fix is ready.
+
+The patch will be accepted if there is broad consensus that it is a
+good thing.  Developers should expect to rework and resubmit patches
+if they don't match the project's coding conventions (see coding.txt)
+or are controversial.
+
+The master branch is regularly built and tested, but is not guaranteed
+to be completely stable. Tags are regularly created to indicate new
+official, stable release versions of Litecoin.
+
+Feature branches are created when there are major new features being
+worked on by several people.
+
+From time to time a pull request will become outdated. If this occurs, and
+the pull is no longer automatically mergeable; a comment on the pull will
+be used to issue a warning of closure. The pull will be closed 15 days
+after the warning if action is not taken by the author. Pull requests closed
+in this manner will have their corresponding issue labeled 'stagnant'.
+
+Issues with no commits will be given a similar warning, and closed after
+15 days from their last activity. Issues closed in this manner will be 
+labeled 'stale'. 
---
+++ src/checkpoints.cpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2011-2012 Litecoin Developers
+// Copyright (c) 2013 Xencoin Developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+
+	// no checkpoint now, can be added in later releases
+    static MapCheckpoints mapCheckpoints =
+            boost::assign::map_list_of
+            (    0, uint256("0xa584407ae5787a82ff973515859e6f6a4774c1304e4f823e0ac971ed0b0ebd5b"))
+            (    1, uint256("0xbcc68282a989455e1243b237190d6d7225bbbfd5254e09373d0d22a158901d60"))
+            (   11, uint256("0x4996583a561e96e7e04d292adf1cae20883cbc4d80f1687ea87ab97d737fe910"))
+			;
+
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (fTestNet) return true; // Testnet has no checkpoints
+
+        MapCheckpoints::const_iterator i = mapCheckpoints.find(nHeight);
+        if (i == mapCheckpoints.end()) return true;
+        return hash == i->second;
+		// return true;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (fTestNet) return 0;
+	
+        return mapCheckpoints.rbegin()->first;
+		// return 0;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (fTestNet) return NULL;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, mapCheckpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+				// return NULL;
+        }
+        return NULL;
+    }
+}
